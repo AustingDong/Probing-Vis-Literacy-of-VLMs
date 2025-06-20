bd27e247453d284e3e1f5bdddf25190360c8b498
AustingDong <a7dong@uwaterloo.ca>
2025-06-20 03:52:31 -0400

    add new test

---
+++ questions/New_test.py
@@ -0,0 +1,73 @@
+new_test_questions=[
+    [
+        "LineChart",
+        "What is the blood sugar level three hours after a meal",
+        "images/New_test/LineChart.png" 
+    ],
+
+    [
+        "BarChart",
+        "Which student has the highest score in midterm?",
+        "images/New_test/BarChart.png"
+    ],
+
+    [
+        "StackedBar",
+        "What is the price of headphone in Japan?",
+        "images/New_test/StackedBar.png"
+    ],
+
+    [
+        "100%StackedBar",
+        "Which country has the largest proportion of mouse price?",
+        "images/New_test/Stacked100.png"
+    ],
+    
+    [
+        "PieChart",
+        "Which stock has the smallest holdings in this portfolio?",
+        "images/New_test/PieChart.png"
+    ],
+    
+    [
+        "Histogram",
+        "Which range of distance of trip people prefers the most?",
+        "images/New_test/Histogram.png"
+    ],
+
+    [
+        "Scatterplot",
+        "What is the weight of the individual that has highest height?", 
+        "images/New_test/Scatterplot.png"
+    ],
+
+    [
+        "AreaChart",
+        "At which month the price of tea is the highest?",
+        "images/New_test/AreaChart.png"
+    ],
+
+    [
+        "StackedArea",
+        "What was the ratio of boys named 'Justin' to boys named 'Kevin' in the 3rd month in the USA?",
+        "images/New_test/StackedArea.png"
+    ],
+
+    [
+        "BubbleChart",
+        "What is the number of employees of the company that has lowest annual income?",
+        "images/New_test/BubbleChart.png"
+    ],
+
+    [ 
+        "Choropleth",
+        "Is the GDP of CA higher than NV?",
+        "images/New_test/Choropleth_New.png"
+    ],
+
+    [
+        "TreeMap",
+        "Which country has the largest population?",
+        "images/New_test/TreeMap.png"
+    ]
+]
